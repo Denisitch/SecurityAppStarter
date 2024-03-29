bea7bae5785914bc2ae06c7742960f22be6adaa1
Denisitch <denisitch@gmail.com>
2024-03-29 15:25:04 +0300

    Initial commit

---
+++ src/main/java/com/denisitch/securityapp/SecurityAppStarterApplication.java
@@ -0,0 +1,13 @@
+package com.denisitch.securityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAppStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityAppStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/denisitch/securityapp/SecurityAppStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denisitch.securityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAppStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
