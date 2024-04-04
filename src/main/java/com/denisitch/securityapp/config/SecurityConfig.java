017d81fc85933f73fa1f85dc73befce5f53fe724
Denisitch <denisitch@gmail.com>
2024-04-04 16:31:04 +0300

    add AuthController and custom login page

--- src/main/java/com/denisitch/securityapp/config/SecurityConfig.java
+++ src/main/java/com/denisitch/securityapp/config/SecurityConfig.java
@@ -8,8 +8,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +29,21 @@ public class SecurityConfig {
                 http.getSharedObject(AuthenticationManagerBuilder.class);
         authenticationManagerBuilder.userDetailsService(personDetailsService);
         return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+                                .requestMatchers("/auth/login", "/error").permitAll()
+                                .anyRequest().authenticated())
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/show-user-info", true)
+                        .failureUrl("/auth/login?error"));
+        return http.build();
     }
 
     @Bean
